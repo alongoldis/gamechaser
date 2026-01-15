4049f6b9eb04f48395e21f55ef96433e3308333c
Alon Goldis <alongoldis@gmail.com>
2026-01-16 00:17:09 +0100

    Fix Vercel prerender error for API routes

--- app/api/browse/trainers/route.ts
+++ app/api/browse/trainers/route.ts
@@ -1,6 +1,6 @@
+export const dynamic = "force-dynamic";
 import { NextResponse } from "next/server";
 import { prisma } from "../../../../lib/db";
-
 export async function GET() {
   const trainers = await prisma.trainer.findMany({
     include: {
